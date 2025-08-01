23fdbf07c3fb365c0c7a684e2dc3301062a9ee41
mouryareddy2006 <llpsjg@gmail.com>
2025-08-01 09:20:51 +0530

    Update joker.py

--- joker.py
+++ joker.py
@@ -1,16 +1,20 @@
 import streamlit as st
-st.title("MOURYA'S FIRST PROJECT ON CODING")
+st.title("MOURYA's file")
 st.header("welcome")
-st.write("this is a basic website which i have made using python")
+st.write("python programme")
 
 name=st.text_input("enter your name:")
 if name:
-   st.success(f"hello,{name}! how are you?,My name is MOURYA how can i help u My name is Mourya. Im currently pursuing my B.Tech and Im from Hyderabad,I have a strong interest in coding and Im always eager to learn new things in the world of technology. Thats one of the reasons Im here — Im looking forward to getting some coding tasks and challenges to improve my skill Im excited to connect with people who share similar interests and grow together")
+   st.success(f"hello,{name}! how are you?,My name is Mourya how can i help you. Im currently pursuing my B.Tech and Im from Hyderabad,I have a strong interest in coding and Im always eager to learn new things in the world of technology. Thats one of the reasons Im here — Im looking forward to getting some coding tasks and challenges to improve my skill Im excited to connect with people who share similar interests and grow together")
 
 
 
 
 number=st.slider("pick your age",0,100)
 st.write(f"your age is {number}")
+
+rat=st.input("enter your email:")
+if rat:
+   st.success(f"we have sent verification code to your email ,{rat}")
 
 
